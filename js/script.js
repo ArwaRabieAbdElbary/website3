4d0e7d313d01eb7fd754e4b1ba1d6ac1826ece45
first <https://github.com/arwarabie/first.git>
2024-02-25 23:52:43 +0200

    change

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
-
-var btn = document.querySelector('.btn')
-function code() {
-    alert("")
-    
+var btnTranslate = document.querySelector('.btn-translate')
+function myFunction(){
+    alert("Do you want to copy this text?")
 }
-btn.dblClick=code
